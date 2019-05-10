cf851b389af0e435e3ab602a61342e9f0fc3c0e2
romkravets <romann.kravets@gmail.com>
2019-05-11 01:13:48 +0300

    Added new slider version

--- src/index/scripst/slider.js
+++ src/index/scripst/slider.js
@@ -1,54 +1,44 @@
+const SHOWED_CLASS = 'showed';
 
 export class Slider {
    constructor(rootElement, imgArray) {
-      //this.slideToggler = this.slideToggler.bind(this);
       this.slidePrev = this.slidePrev.bind(this);
       this.slideNext = this.slideNext.bind(this);
       this.rootElement = rootElement;
       this.imgArray = imgArray;
       this.btnPrev = document.querySelector('.slider__btns_prev');
       this.btnNext = document.querySelector('.slider__btns_next');
-      this.images = document.querySelectorAll('.slider__img');
+      this.images = document.querySelectorAll('.slider__image');
       this.currentIndex = 0;
-      this.imgDuration = 2000;
+      this.imgDuration = 1000;
       this.render();
       this.slideNext();
       this.slidePrev()
    }
 
-   //  slideToggler() {
-   //     this.rootElement.src = this.imgArray[this.currentIndex];
-   //     this.currentIndex++;
-   //    if (this.currentIndex === this.imgArray.length) {
-   //       this.currentIndex = 0;
-   //    }
-   //    //setTimeout(this.slideToggler, this.imgDuration);
-    //}
-
    slidePrev() {
       this.btnPrev.addEventListener('click', () => {
-         this.images[this.currentIndex].style.display = 'none';
+         this.images[this.currentIndex].classList.remove(SHOWED_CLASS);
          this.currentIndex--;
 
          if(this.currentIndex < 0) {
             this.currentIndex = this.images.length - 1;
          }
-         this.images[this.currentIndex].style.display = 'block';
+         this.images[this.currentIndex].classList.add(SHOWED_CLASS)
       })
    }
 
    slideNext() {
       this.btnNext.addEventListener('click', () => {
-         this.images[this.currentIndex].style.display = 'none';
+         this.images[this.currentIndex].classList.remove(SHOWED_CLASS);
          this.currentIndex++;
 
          if(this.currentIndex >= this.images.length) {
             this.currentIndex = 0;
          }
-         this.images[this.currentIndex].style.display = 'block';
+         this.images[this.currentIndex].classList.add(SHOWED_CLASS);
       })
       }
-
 
    render() {
 
