d2941008d992162b7e0285977a9854a3bce5588d
romkravets <romann.kravets@gmail.com>
2019-05-10 20:02:11 +0300

    Added simple slider

--- src/index/index.js
+++ src/index/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 import { Slider } from './scripst/slider';
 
-new Slider('imageSlider', [
+new Slider(document.querySelector('#imageSlider'), [
    'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?cs=srgb&dl=beach-exotic-holiday-248797.jpg&fm=jpg',
    'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?cs=srgb&dl=dawn-hd-wallpaper-landscape-36717.jpg&fm=jpg',
    'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?cs=srgb&dl=beautiful-blur-bright-326055.jpg&fm=jpg',
--- src/index/scripst/slider.js
+++ src/index/scripst/slider.js
@@ -1,16 +1,25 @@
 
 export class Slider {
    constructor(rootElement, imgArray) {
+      this.slideToggler = this.slideToggler.bind(this);
       this.rootElement = rootElement;
       this.imgArray = imgArray;
       this.currentIndex = 0;
       this.imgDuration = 2000;
       this.render();
+      this.slideToggler();
+   }
+
+   slideToggler() {
+      this.rootElement.src = this.imgArray[this.currentIndex];
+      this.currentIndex++;
+      if (this.currentIndex === this.imgArray.length) {
+         this.currentIndex = 0;
+      }
+      setTimeout(this.slideToggler, this.imgDuration);
    }
 
    render() {
-      console.log('Hello World');
-      console.log(this.rootElement);
-      console.log(this.imgArray);
+
    }
 }
